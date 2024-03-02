b7f87b8638e6c5a884947052b4dd5d6f67da5c51
mnmonherdene1234 <mnmonherdene1234@gmail.com>
2024-03-03 00:59:54 +0800

    http server on

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func qrHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+}
 
 func main() {
-	fmt.Println("Hello QR code API")
+	http.HandleFunc("/api/qr", qrHandler)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Server is running on port 8080")
+	}
 }
