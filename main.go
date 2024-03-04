728e92981a78e79a6436585a0b4a626a12f0dcfb
mnmonherdene <mnmonherdene1234@gmail.com>
2024-03-04 15:31:35 +0800

    api path done

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 	"net/http"
 )
 
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/api/qr" {
+		qrHandler(w, r)
+	} else {
+		// JSON response
+		w.Header().Set("Content-Type", "application/json")
+		_, err := w.Write([]byte(`{"error": "Not found"}`))
+		if err != nil {
+			return
+		}
+	}
+}
+
 func qrHandler(w http.ResponseWriter, r *http.Request) {
 	png, err := qrcode.Encode("https://example.org", qrcode.Medium, 256)
 	if err != nil {
@@ -14,11 +27,14 @@ func qrHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "image/png")
-	w.Write(png)
+	_, err = w.Write(png)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
-	http.HandleFunc("/", qrHandler)
+	http.HandleFunc("/", requestHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	} else {
